bc0231c300d2e3959413d51054f67ef5d7d6fd5e
Bar Goldenstein <bargolden12@gmail.com>
2023-06-09 19:00:21 +0300

    css improvements!!! yay!!!!!!!!

--- src/App.jsx
+++ src/App.jsx
@@ -32,6 +32,7 @@ function App() {
 		<>
 			<div className="console-header">
 				<span className="console-title">Build Monitor</span>
+				<span className="console-author">By DetKewlDog</span>
 			</div>
 			<div className="console-body">
 				<Config
--- src/services/LogAccess.js
+++ src/services/LogAccess.js
@@ -7,39 +7,41 @@ const DEBUG_LOG_STRING = '(Filename: C:\\buildslave\\unity\\build\\Runtime/Expor
 const MAX_LOGGED_LINES_COUNT = 5000
 
 class LogAccess {
+    static #parseFetchedLogs(logs) {
+        if (logs === '') return;
+        const lines = logs.split('\n');
+        var isError = false;
+        var updatedLog = lines.map((line, index) => {
+            // Error logs end with a specific string
+            if (isError && line.includes(EXCEPTION_END_STRING)) isError = false;
+
+            // Log is an exception / error
+            if (isError || line.includes(EXCEPTION_STRING)) {
+                isError = true;
+                return { content: line, type: 'error' };
+            }
+            // Log was printed using Debug.Log
+            else if (line.includes(DEBUG_LOG_STRING)
+                || (index + 1 < lines.length
+                    && lines[index + 1].includes(DEBUG_LOG_STRING))) {
+                return { content: line, type: 'print' };
+            }
+            // Log has been written automatically by Unity (Debug.Asset iirc?)
+            else return { content: line, type: '' };
+        });
+        // get the last MAX_LOGGED_LINES_COUNT lines
+        if (updatedLog.length >= MAX_LOGGED_LINES_COUNT) {
+            updatedLog = updatedLog.slice(-MAX_LOGGED_LINES_COUNT);
+        }
+        return updatedLog;
+    }
+
     static async fetchLogs(url, online) {
         if (url === '' || !online) return [];
         return axios.get(url)
             .then(result => result.data)
-            .then(result => {
-                if (result == '') return;
-                const lines = result.split('\n');
-                var isError = false;
-                var updatedLog = lines.map((line, index) => {
-                    // Error logs end with a specific string
-                    if (isError && line.includes(EXCEPTION_END_STRING)) isError = false;
-
-                    // Log is an exception / error
-                    if (isError || line.includes(EXCEPTION_STRING)) {
-                        isError = true;
-                        return { content: line, type: 'error' };
-                    }
-                    // Log was printed using Debug.Log
-                    else if (line.includes(DEBUG_LOG_STRING)
-                        || (index + 1 < lines.length
-                            && lines[index + 1].includes(DEBUG_LOG_STRING))) {
-                        return { content: line, type: 'print' };
-                    }
-                    // Log has been written automatically by Unity (Debug.Asset iirc?)
-                    else return { content: line, type: '' };
-                });
-                // get the last MAX_LOGGED_LINES_COUNT lines
-                if (updatedLog.length >= MAX_LOGGED_LINES_COUNT) {
-                    updatedLog = updatedLog.slice(-MAX_LOGGED_LINES_COUNT);
-                }
-                return updatedLog;
-            }
-        ).catch(error => console.log(error));
+            .then(result => LogAccess.#parseFetchedLogs(result))
+            .catch(error => console.log(error));
     }
 }
 
